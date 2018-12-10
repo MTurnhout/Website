916d4b60e145c7a00e10309b905581368e233730
Martijn Turnhout <m_turnhout@hotmail.com>
2018-12-10 02:09:46 +0100

    Handle client and webapi routing Add blog components Add todo to client

--- src/Clients/Angular/src/app/blog/blog.module.ts
+++ src/Clients/Angular/src/app/blog/blog.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BlogComponent } from './blog.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BlogComponent, PostListComponent, PostComponent],
   imports: [
     CommonModule
   ]
--- src/Website.Api/Startup.cs
+++ src/Website.Api/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,12 +19,16 @@ namespace Mt.Website.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _environment;
+
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
-            Configuration = configuration;
+            _configuration = configuration;
+            _environment = environment;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,14 +36,14 @@ namespace Mt.Website.Api
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddBusinessServices(options => 
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"))
             );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (_environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -47,8 +53,25 @@ namespace Mt.Website.Api
                 app.UseHttpsRedirection();
             }
 
+            // Client
             app.UseFileServer();
+            app.MapWhen(
+                context => !context.Request.Path.StartsWithSegments("/api"),
+                HandleSpa
+            );
+            
+            // Api
             app.UseMvc();
+        }
+
+
+        private void HandleSpa(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync(Path.Combine(_environment.WebRootPath,"index.html"));
+            });
         }
     }
 }

38c96c71280559b021c44f5b6e042034998eb53d
Martijn Turnhout <martijn.turnhout@cerrix.com>
2019-07-12 11:40:22 +0200

    Creating login page

--- src/Clients/Angular/src/app/login/login.component.ts
+++ src/Clients/Angular/src/app/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  formValidation: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.initValidation();
   }
 
+  initValidation() {
+    this.formValidation = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*[\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+]{8,}$/
+        )
+      ])
+    });
+  }
+
+  login() {
+    if (!this.formValidation.valid) {
+      this.formValidation.markAllAsTouched();
+      return;
+    }
+  }
 }
--- src/Clients/Angular/src/app/login/login.module.ts
+++ src/Clients/Angular/src/app/login/login.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from "@angular/common";
 import { LoginRoutingModule } from "./login-routing.module";
 import { LoginComponent } from "./login.component";
 import { SharedModule } from "../shared/shared.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, SharedModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoginRoutingModule,
+    SharedModule
+  ]
 })
 export class LoginModule {}

eea38a0fb1d3832dfc3ad12ab50ea5c030c09cc0
Martijn Turnhout <martijn.turnhout@cerrix.com>
2019-07-12 11:58:27 +0200

    Remove lazy loaded blog module from app module

--- src/Clients/Angular/src/app/app.module.ts
+++ src/Clients/Angular/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
-import { BlogModule } from "./blog/blog.module";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
@@ -16,8 +15,7 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule,
-    BlogModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]

323349517bcd63b7bd34577493cae7d061b78e76
Martijn Turnhout <martijn.turnhout@cerrix.com>
2019-11-12 14:11:57 +0100

    Add applicationInsights settings to prod

--- src/Clients/Angular/src/environments/environment.prod.ts
+++ src/Clients/Angular/src/environments/environment.prod.ts
@@ -8,5 +8,8 @@ export const environment = {
     tenantId: "bfc60a20-4601-4b3b-aa55-2ba676b14976",
     clientId: "ba0156a1-daaa-4b6d-990a-a73283a95a4a",
     webApiId: "0be261fe-0e96-4163-b07d-fbac0059c339"
+  },
+  applicationInsights: {
+    instrumentationKey: "850af97e-3c89-4da4-aee3-79bfa6cb9a8c"
   }
 };

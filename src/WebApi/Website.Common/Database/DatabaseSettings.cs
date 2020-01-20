1122e1fe84437bbadfd6b5b77ec107f1ac1c98a1
M. Turnhout <m_turnhout@hotmail.com>
2020-01-21 00:39:52 +0100

    Add common project

---
+++ src/WebApi/Website.Common/Database/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Website.Common.Database
+{
+    public class DatabaseSettings
+    {
+        public DatabaseType DatabaseType { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ src/WebApi/Website.Common/Database/DatabaseType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Website.Common
+{
+    public enum DatabaseType
+    {
+        MsSql
+    }
+}

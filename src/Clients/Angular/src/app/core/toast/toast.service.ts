e0805e60546c9338fec6e75b17572d45989e39e7
M. Turnhout <m_turnhout@hotmail.com>
2020-01-21 21:12:28 +0100

    Update oidc-client and fix toast issue

--- src/Clients/Angular/src/app/core/toast/toast.service.ts
+++ src/Clients/Angular/src/app/core/toast/toast.service.ts
@@ -4,7 +4,7 @@ import { Injectable, TemplateRef } from "@angular/core";
   providedIn: "root"
 })
 export class ToastService {
-  private toasts: any[] = [];
+  public toasts: any[] = [];
 
   public show(textOrTpl: string | TemplateRef<any>, options: any = {}) {
     this.showToast(textOrTpl, options);
